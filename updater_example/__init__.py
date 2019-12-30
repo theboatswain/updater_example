44d4051f1797bd2788f0a5ef35e0d02aec094090
glmanhtu <glmanhtu@gmail.com>
2019-12-30 23:03:39 +0100

    first commit

---
+++ updater_example/__init__.py
@@ -0,0 +1,5 @@
+from updater_example.main import run
+
+
+def main():
+    run()
---
+++ updater_example/main.py
@@ -0,0 +1,55 @@
+import os
+import subprocess
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        central_widget = QWidget(self)
+        main_layout = QVBoxLayout(central_widget)
+        self.check = QPushButton(central_widget)
+        self.check.setText("Check for update")
+        self.check.clicked.connect(self.openAutoUpdater)
+        main_layout.addWidget(self.check)
+        self.setCentralWidget(central_widget)
+
+    def findAutoUpdaterLocation(self):
+        executable = sys.executable
+        (all_last_dir, folder) = os.path.split(executable)
+        (all_before_last_dir, last_dir) = os.path.split(all_last_dir)
+        (all_before_before_last_dir, before_last_dir) = os.path.split(all_before_last_dir)
+        if last_dir == 'MacOS' and before_last_dir == 'Contents':
+            folder = all_before_before_last_dir
+        else:
+            folder = all_last_dir
+        return folder
+
+    def openAutoUpdater(self):
+        folder = self.findAutoUpdaterLocation()
+        if sys.platform.startswith('win'):
+            process = subprocess.run([os.path.join(folder, 'AutoUpdater.exe')])
+        else:
+            process = subprocess.run([os.path.join(folder, 'AutoUpdater')])
+        try:
+            process.check_returncode()
+
+            # Restart app
+            os.execlp(sys.executable, *sys.argv)
+        except subprocess.CalledProcessError as e:
+            print('Exception occurred, e: ', e)
+
+
+def run():
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    run()
